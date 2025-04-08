1232d389342b438b4259579b38b841352f48c405
Gabriel <gaboquintana10@gmail.com>
2025-04-08 09:55:08 -0600

    Termine la card

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router,Routes,Route } from "react-router"
 
-// Corrige la ruta de importación según donde esté ubicado tu archivo
-// Si está en la carpeta 'components' dentro de 'src':
-import RickAndMortyApp from './components/RickAndMortyApp.jsx'
+
+import Nav from './components/Nav/Nav.jsx'
+import DragonBall from './pages/DragonBall.jsx';
+import Morty from './pages/Morty.jsx'
+import Home from './pages/Home.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      
-      {/* Añade el componente dentro del return */}
-      <RickAndMortyApp />
-    </>
-  )
+return (
+<>
+<Router>
+<Nav /><Routes>
+<Route path="/" element={<Home />} />
+<Route path="/dragon" element={<DragonBall/>} />
+<Route path="/morty" element={<Morty/>} />
+</Routes>
+</Router>
+</>
+)
 }
-
 export default App
